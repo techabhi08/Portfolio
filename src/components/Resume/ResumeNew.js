6c7574eb0ed221bada77164a332f9aa9287ab249
techabhi08 <garg.abhinay8602@gmail.com>
2023-05-04 18:42:59 +0530

    Updated the resume file

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Abhinay_Garg_Resume.pdf";
+import pdf from "../../Assets/../Assets/Abhinay_Garg_Resume_4thMay.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/techabhi08/portfolio/master/src/Assets/Abhinay_Garg_Resume.pdf";
+  "https://raw.githubusercontent.com/techabhi08/portfolio/master/src/Assets/Abhinay_Garg_Resume_4thMay.pdf";
 
 
 function ResumeNew() {
